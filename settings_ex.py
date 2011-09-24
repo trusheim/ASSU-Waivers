0111952134d64c9d0c20de568ddbb216dc09372b
Stephen Trusheim <strusheim@gmail.com>
2011-09-23 18:15:11 -0700

    Settings updates after trying to upload this to mod_python

--- settings_ex.py
+++ settings_ex.py
@@ -6,6 +6,7 @@ from settings import *
 MEDIA_ROOT = '/Users/stephen/Desktop/waivers/media/'
 TEMPLATE_DIRS = ('/Users/stephen/Desktop/waivers/templates',)
 
+SECRET_KEY = '4jd607m#-)re#asdasgasdfgakd7zj9**7o_+3%=v4)h9523+%1o$a3_fm2yl'
 WEBAUTH_SHARED_SECRET = 'test'
 WEBAUTH_URL = 'https://www.stanford.edu/~trusheim/cgi-bin/wa-authenticate-test.php'
 BASE_URL = 'http://localhost:8000/'
