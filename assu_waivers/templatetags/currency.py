59bab44567073d17c1f00c28016b80ed6a251698
Stephen Trusheim <strusheim@gmail.com>
2011-09-27 22:57:06 -0700

    Updates to currency filter, which still sometimes has issues?

--- assu_waivers/templatetags/currency.py
+++ assu_waivers/templatetags/currency.py
@@ -1,9 +1,9 @@
 from django import template
 import locale
-locale.setlocale(locale.LC_ALL, 'en_US.UTF-8') # necessary in mod_python
 register = template.Library()
  
 # http://djangosnippets.org/snippets/552/
 @register.filter()
 def currency(value):
+    locale.setlocale(locale.LC_ALL, 'en_US.UTF-8') # necessary in mod_python
     return locale.currency(value, grouping=True)
